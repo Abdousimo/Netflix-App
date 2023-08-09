7116690c591a42f29362d5843e07064366d7ec84
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2023-08-09 23:11:01 +0000

    initial commit

---
+++ netflix/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <section className='bg-black w-full h-20 flex items-center justify-between px-2 md:px-7'>
+             <h1 className='text-red-700 font-bold text-xl md:text-3xl'>NETFLIX</h1>
+             <div>
+                <button className='text-white pr-3'>Sign In</button>
+                <button className='bg-red-700 text-white px-6 py-2 rounded-lg'>Sign Up</button>
+             </div>
+        </section>
+    );
+}
+
+export default Navbar;
