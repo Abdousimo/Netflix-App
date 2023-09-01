9a743a8487e76cae54f16ce5b4f5986527430bdd
Abdousimo <96550431+Abdousimo@users.noreply.github.com>
2023-09-01 23:52:06 +0000

    initial commit

--- components/Movie.jsx
+++ components/Movie.jsx
@@ -3,11 +3,32 @@ import React,{useState} from 'react';
 import {FaHeart,FaRegHeart} from 'react-icons/fa'
 import {BiMoviePlay} from 'react-icons/bi'
 import { UserAuth } from '@/context/AuthContext';
+import { db } from '@/firebase';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
 
 const Movie = ({item}) => {
    
     const [like, setLike] = useState(false)
+    const [saved, setSaved] = useState(false)
     const {user} =  UserAuth()
+
+    const movieID = doc(db, 'users', `${user?.email}`)
+
+    const saveShow = async () => {
+        if(user?.email){
+            setLike(!like)
+            setSaved(true)
+            await updateDoc(movieID,{
+                savedShows : arrayUnion({
+                    id: item.id,
+                    title: item.title,
+                    img: item.backdrop_path
+                })
+            })
+        }else{
+            alert('Please log in to save a movie !!')
+        }
+    }
 
     return (
         <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
@@ -16,7 +37,7 @@ const Movie = ({item}) => {
                     <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center max-w-full'>
                         {item?.title}
                     </p>
-                    <p onClick={()=> setLike(!like)}>
+                    <p onClick={saveShow}>
                            {like ? <FaHeart className='absolute top-4 left-4 text-gray-400'/> : <FaRegHeart className='absolute top-4 left-4 text-gray-400'/>}
                     </p>
                     { user?.email ? 
@@ -27,7 +48,7 @@ const Movie = ({item}) => {
                          null
                          }
                 </div>                
-            </div>
+        </div>
     );
 }
 
--- pages/Account.jsx
+++ pages/Account.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ProtectRoute from '@/components/ProtectRoute';
 import Head from 'next/head';
+import SavedShows from '@/components/SavedShows';
+
 
 function Account(props) {
     return (
@@ -9,7 +11,15 @@ function Account(props) {
                 <title>Account</title>
             </Head>
             <ProtectRoute>
-               <></> 
+               <div className='w-full text-white'>
+                     <img className='w-full h-[400px] object-cover' src='/netflix-code.png' alt='/' />
+                     <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
+                        <div className='absolute top-[20%] p-4 md:p-8'>
+                            <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+                        </div>
+                     </div> 
+                     <SavedShows/> 
+               </div> 
             </ProtectRoute>
         </>
         
